6eff1848020d17f81db0c13c40b2dbce4326fb97
natdoly <61319301+natdoly@users.noreply.github.com>
2023-12-05 15:15:15 +0100

    11 - Add studentIT + step test in gh actions

---
+++ src/test/resources/data.sql
@@ -0,0 +1,7 @@
+ DELETE FROM student;
+INSERT INTO student (name, age) VALUES ('Nathan', 23);
+INSERT INTO student (name, age) VALUES ('Théo', 20);
+INSERT INTO student (name, age) VALUES ('Jeanne', 40);
+INSERT INTO student (name, age) VALUES ('Sebastien', 35);
+INSERT INTO student (name, age) VALUES ('Pauline', 52);
+INSERT INTO student (name, age) VALUES ('Gerard', 29);
